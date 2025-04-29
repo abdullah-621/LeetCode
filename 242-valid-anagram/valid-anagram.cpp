dac89d563efa96b973551835d1e1a6fd09883d35
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-04-29 19:14:13 +0600

    Time: 5 ms (31.39%) | Memory: 10 MB (11.91%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        if(s.length() != t.length()) return false;
+
+        unordered_map<char,int>freq;
+
+        for(char ch : s){
+            freq[ch]++;
+        }
+
+        for(auto ch : t){
+            freq[ch]--;
+
+            if(freq[ch] < 0) return false;
+        }
+
+        return true;
+    }
+};

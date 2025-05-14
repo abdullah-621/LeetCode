dd3b2750a3fbe1720b448d37cb1d5b0617f9a4a7
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-05-14 15:56:22 +0600

    Time: 0 ms (100.00%) | Memory: 11.6 MB (91.24%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -2,32 +2,54 @@ class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
 
-        int n = students.size();
+        // int n = students.size();
 
-        queue<int>stu;
-        stack<int>san;
+        // queue<int>stu;
+        // stack<int>san;
+
+        // for(int i = 0; i < students.size(); i++){
+        //     stu.push(students[i]);
+        //     san.push(sandwiches[n - 1 - i]);
+        // }
+
+        // int count = 0;
+
+        // while(count < stu.size()){
+        //     if(stu.front() == san.top()){
+        //         stu.pop();
+        //         san.pop();
+        //         count = 0;
+        //     }
+        //     else{
+        //         count++;
+        //         int last = stu.front();
+        //         stu.pop();
+        //         stu.push(last);
+        //     }
+        // }
+
+        // return stu.size();
+
+        int zeros = 0;
+        int ones = 0;
 
         for(int i = 0; i < students.size(); i++){
-            stu.push(students[i]);
-            san.push(sandwiches[n - 1 - i]);
+            if(students[i] == 0) zeros++;
+            else ones++;
         }
 
-        int count = 0;
-
-        while(count < stu.size()){
-            if(stu.front() == san.top()){
-                stu.pop();
-                san.pop();
-                count = 0;
-            }
+        for(int i = 0 ; i < sandwiches.size(); i++){
+            if(sandwiches[i] == 0 && zeros != 0){
+                zeros--;
+            } 
+            else if(sandwiches[i] == 1 && ones != 0){
+                ones--;
+            } 
             else{
-                count++;
-                int last = stu.front();
-                stu.pop();
-                stu.push(last);
+                return zeros + ones;
             }
         }
 
-        return stu.size();
+        return zeros + ones;
     }
 };

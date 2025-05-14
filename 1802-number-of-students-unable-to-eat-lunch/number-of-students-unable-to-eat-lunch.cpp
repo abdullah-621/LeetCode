20586b8c62012417f891a8954494cad23c8eecac
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-05-14 15:42:50 +0600

    Time: 0 ms (100.00%) | Memory: 11.9 MB (13.64%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -1,39 +1,15 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        
-        // int i = 0;
-        // int j = 0;
 
-        // int student = students.size();
-        // int count = 0;
-
-        // while(count < student){
-        //     if(students[i] == sandwiches[j]){
-
-        //         j++;
-        //         count = 0;
-                
-        //     }
-        //     else{
-        //         int back = students[i];
-        //         students.push_back(back);
-        //         count++;
-        //     }
-        //     i++;
-        //     if(i >= students.size()) i = 0;
-        // }
-
-        // return student - j;
+        int n = students.size();
 
         queue<int>stu;
         stack<int>san;
 
         for(int i = 0; i < students.size(); i++){
             stu.push(students[i]);
-        }
-        for(int i = sandwiches.size() - 1; i >=0 ; i--){
-            san.push(sandwiches[i]);
+            san.push(sandwiches[n - 1 - i]);
         }
 
         int count = 0;

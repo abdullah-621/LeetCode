c62b10c3c3fbf347971897221988e872ac832d26
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-05-18 14:30:30 +0600

    Time: 0 ms (100.00%) | Memory: 33.1 MB (29.02%) - LeetSync

--- 3194-find-words-containing-character/find-words-containing-character.cpp
+++ 3194-find-words-containing-character/find-words-containing-character.cpp
@@ -1,25 +1,41 @@
+// class Solution {
+// public:
+// bool findX(string s, char x){
+
+//     for(char ch : s){
+//         if(ch == x){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+//     vector<int> findWordsContaining(vector<string>& words, char x) {
+        
+//         vector<int>ans;
+
+//         for(int i = 0; i < words.size(); i++ ){
+//             string s = words[i];
+//            bool flag = findX(s,x);
+
+//            if(flag) ans.push_back(i);
+//         }
+
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-bool findX(string s, char x){
-
-    for(char ch : s){
-        if(ch == x){
-            return true;
-        }
-    }
-    return false;
-}
     vector<int> findWordsContaining(vector<string>& words, char x) {
-        
-        vector<int>ans;
-
-        for(int i = 0; i < words.size(); i++ ){
-            string s = words[i];
-           bool flag = findX(s,x);
-
-           if(flag) ans.push_back(i);
+        vector<int> idx;
+        for (int i = 0; i < words.size(); i++) {
+            for (char ch : words[i]) {
+                if (ch == x) {
+                    idx.push_back(i);
+                    break;
+                }
+            }
         }
-
-        return ans;
+        return idx;
     }
 };

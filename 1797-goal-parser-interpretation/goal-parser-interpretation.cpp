e3c746d0c24b35bc10822c27107b5ce13cb17ece
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-05-21 09:01:20 +0600

    Time: 0 ms (100.00%) | Memory: 8.3 MB (47.20%) - LeetSync

---
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string interpret(string command) {
+        
+        string s = "";
+
+        for(int i = 0; i < command.size(); i++){
+            if(command[i] == 'G'){
+                s += "G";
+            }
+            else if(command[i] == '(' && command[i + 1] == ')'){
+                s += "o";
+                i+= 1;
+            }
+            else if(command[i] == '(' && command[i + 1] =='a' && command[i + 2] == 'l' && command[i + 3] == ')'){
+                s += "al";
+                i+= 3;
+            }
+        }
+
+        return s;
+    }
+};

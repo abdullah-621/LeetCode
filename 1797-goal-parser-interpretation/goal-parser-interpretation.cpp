34a076ffe831307f5074ae0b2591f3949df4e0d7
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-05-21 09:08:23 +0600

    Time: 0 ms (100.00%) | Memory: 8.1 MB (93.41%) - LeetSync

--- 1797-goal-parser-interpretation/goal-parser-interpretation.cpp
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.cpp
@@ -21,3 +21,22 @@ public:
         return s;
     }
 };
+
+
+// string interpret(string s) {
+//         unordered_map<string, string> d = 
+//         {
+//             { "(al)" , "al"},
+//             { "()" , "o" }, 
+//             { "G" , "G" },
+//         };
+//         string tmp = "", res = "";
+//         for(char c: s){
+//             tmp+=c;
+//             if(d.find(tmp)!=d.end()){
+//                 res += d[tmp];
+//                 tmp = "" ;
+//             }
+//         }
+//         return res;
+//     }

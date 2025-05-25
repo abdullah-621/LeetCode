f262e291b9a6f0a555f056209279aaf2fbf7a69d
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-05-25 14:41:02 +0600

    Time: 0 ms (100.00%) | Memory: 8.5 MB (69.26%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        stack<char>st;
+        // stack<char>st;
+
+        // for(char ch : s){
+        //     if(!isdigit(ch)){
+        //         st.push(ch);
+        //     }
+        //     else{
+        //         if(!st.empty())
+        //         st.pop();
+        //     }
+        // }
+
+        // string ans = "";
+        // while(!st.empty()){
+        //     ans += st.top();
+        //     st.pop();
+        // }
+        // reverse(ans.begin(), ans.end());
+
+        // return ans;
+
+        string stk;
 
         for(char ch : s){
-            if(!isdigit(ch)){
-                st.push(ch);
-            }
-            else{
-                if(!st.empty())
-                st.pop();
-            }
-        }
+            if(isdigit(ch)){
 
-        string ans = "";
-        while(!st.empty()){
-            ans += st.top();
-            st.pop();
-        }
-        reverse(ans.begin(), ans.end());
-
-        return ans;
+                if(!stk.empty())stk.pop_back();
+             }
+                else{
+                    stk.push_back(ch);
+                }
+            }
+        return stk;
     }
 };

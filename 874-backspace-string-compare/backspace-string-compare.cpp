b0c34954c2c8c39bdf61874c9e1a974b60a4d256
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-05-31 12:24:37 +0600

    Time: 0 ms (100.00%) | Memory: 8.5 MB (47.60%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.cpp
+++ 874-backspace-string-compare/backspace-string-compare.cpp
@@ -6,23 +6,16 @@ public:
        for(char ch : str){
         if(ch != '#') ans += ch;
 
-        else{
-
-            if(ans.size() > 0) ans.erase(ans.end() - 1);
-
+        else
+        {
+            if(!ans.empty()) ans.pop_back();
         } 
        }
 
        return ans;
     }
     bool backspaceCompare(string s, string t) {
-        string s1 = StringSizeAfterOperation(s);
-        string t1 = StringSizeAfterOperation(t);
-
-        // for(int i = 0; i < s.size(); i++){
-        //     if(s1[i] != t1[i]) return false;
-        // }
-
-        return s1 == t1;
+        
+        return StringSizeAfterOperation(s) == StringSizeAfterOperation(t);
     }
 };

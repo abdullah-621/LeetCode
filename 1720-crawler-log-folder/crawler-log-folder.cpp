c19773b245bf8867fd19d60a796fcb4f0a562b86
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-05-31 11:47:35 +0600

    Time: 1 ms (14.53%) | Memory: 14.1 MB (65.24%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
-        stack<string>st;
+        // stack<string>st;
 
-        for(int i = 0; i < logs.size(); i++){
-            if(logs[i] == "../"){
-                if(!st.empty()) st.pop();
-            } 
+        // for(int i = 0; i < logs.size(); i++){
+        //     if(logs[i] == "../"){
+        //         if(!st.empty()) st.pop();
+        //     } 
 
-            else if(logs[i] == "./") continue;
+        //     else if(logs[i] == "./") continue;
 
-            else st.push(logs[i]);
+        //     else st.push(logs[i]);
+        // }
+
+        // return st.size();
+
+        int ans = 0;
+
+        for(auto log : logs){
+            if(log == "../"){
+                if(ans > 0) ans--;
+            }
+            else if(log != "./") ans++;
         }
 
-        return st.size();
+        return ans;
     }
 };

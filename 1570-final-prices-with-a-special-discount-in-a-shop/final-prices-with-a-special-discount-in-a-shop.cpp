f5aa4fe79811bb571750f00100b18b8f007b37d7
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-05-31 10:44:49 +0600

    Time: 1 ms (18.15%) | Memory: 14.2 MB (26.28%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -2,15 +2,27 @@ class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
 
-        for(int i = 0; i < prices.size(); i++){
-            for(int j = i + 1; j < prices.size(); j++){
-                if(prices[i] >= prices[j]){
-                    prices[i] = prices[i] - prices[j];
-                    break;
-                }
-            }
-        }
+        // for(int i = 0; i < prices.size(); i++){
+        //     for(int j = i + 1; j < prices.size(); j++){
+        //         if(prices[i] >= prices[j]){
+        //             prices[i] = prices[i] - prices[j];
+        //             break;
+        //         }
+        //     }
+        // }
 
+        // return prices;
+
+        // vector<int>ans(prices);
+        stack<int>st;
+
+        for(int i = 0; i < prices.size(); i++){
+            while(!st.empty() && prices[st.top()] >= prices[i]){
+                prices[st.top()] = prices[st.top()] - prices[i] ;
+                st.pop();
+            }
+            st.push(i);
+        }
         return prices;
     }
 };

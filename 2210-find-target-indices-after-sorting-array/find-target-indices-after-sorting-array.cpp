47ffc3aa08246bdd1b69156e91ca3aa17783d2c6
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-06-10 22:17:32 +0600

    Time: 0 ms (100.00%) | Memory: 15.2 MB (11.18%) - LeetSync

--- 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.cpp
@@ -1,3 +1,23 @@
+// class Solution {
+// public:
+//     vector<int> targetIndices(vector<int>& nums, int target) {
+//         sort(nums.begin(), nums.end());
+//         vector<int>ans;
+
+//         int i = 0;
+//         int j = nums.size() - 1;
+
+//         while(i < j){
+//             if(nums[i] == target) ans.push_back(i);
+//             if(nums[j] == target) ans.push_back(j);
+//             i++;
+//             j--;
+//         }
+//         return ans;
+
+//     }
+// };
+
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {

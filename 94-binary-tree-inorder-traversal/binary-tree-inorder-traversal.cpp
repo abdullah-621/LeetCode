32701c0d867ce27c054d2ab16cc50f51eb6d8165
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-08-05 11:21:19 +0600

    Time: 0 ms (100.00%) | Memory: 10.9 MB (63.20%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -11,16 +11,41 @@
  */
 class Solution {
 public:
-    void inorderTraversal(vector<int>&ans,TreeNode* root)
-    {
-        if (root == nullptr) return;
-        inorderTraversal(ans,root->left);
-        ans.push_back(root->val);
-        inorderTraversal(ans,root->right);
-    }
+    // void inorderTraversal(vector<int>&ans,TreeNode* root)
+    // {
+    //     if (root == nullptr) return;
+    //     inorderTraversal(ans,root->left);
+    //     ans.push_back(root->val);
+    //     inorderTraversal(ans,root->right);
+    // }
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int>ans;
-        inorderTraversal(ans,root);
+        
+
+        // inorderTraversal(ans,root);
+
+        // iterative method
+        stack<TreeNode*>st;
+        TreeNode *curr = root;
+
+        while(curr != nullptr || !st.empty())
+        {
+            // 1. Left subtree যতদূর সম্ভব traverse করি
+            while(curr != nullptr)
+            {
+                st.push(curr);
+                curr = curr->left;
+            }
+
+            // 2. এখন যেটা top এ আছে — সেটাই leftmost node
+            curr = st.top();
+            st.pop();
+            ans.push_back(curr->val);
+
+            // 4. তারপর তার right subtree তে যাই
+            curr = curr->right;
+        }
+
         return ans;
     }
 };

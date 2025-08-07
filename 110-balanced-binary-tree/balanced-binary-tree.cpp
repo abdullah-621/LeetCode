d3fd83b751b392c0775de979fe785c337999d309
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-08-07 10:24:04 +0600

    Time: 0 ms (100.00%) | Memory: 23.2 MB (23.05%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -20,16 +20,47 @@ public:
         if(left_height == -1) return -1;
 
         int right_height = getBalance(root->right);
+        
         if(right_height == -1) return -1;
 
         int balance = left_height - right_height;
 
         if (balance < -1 || balance > 1) return -1;
 
-        return 1 + max(left_height, right_height);
+        return 1 + max(left_height, right_height);  // height update
     }
     bool isBalanced(TreeNode* root) 
     {
         return getBalance(root) != -1;
     }
 };
+
+// class Solution {
+// public:
+//     int getHeight(TreeNode *root)
+//     {
+//         if(root == nullptr) return 0;
+
+//         int left_height = getHeight(root->left);
+//         int right_height = getHeight(root->right);
+
+//         return 1 + max(left_height, right_height);
+//     }
+//     // int getBalance(TreeNode *root)
+//     // {
+//     //     return getHeight(root->left) - getHeight(root->right);
+//     // }
+//     bool isBalanced(TreeNode* root) 
+//     {
+//         if(root == nullptr) return true;
+
+//         int left_height = getHeight(root->left);
+//         int right_height = getHeight(root->right);
+
+//         int balance = left_height - right_height;
+
+//         if (balance < -1 || balance > 1) return false;
+
+//         return isBalanced(root->left) && isBalanced(root->right);
+//     }
+// };

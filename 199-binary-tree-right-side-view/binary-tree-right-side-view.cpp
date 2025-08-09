ca64493245e7d26399cc4110794acc2431afa606
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-08-09 10:05:10 +0600

    Time: 0 ms (100.00%) | Memory: 14.9 MB (83.06%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -6,37 +6,58 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    vector<int> rightSideView(TreeNode* root) 
-    {
-        vector<int>result;
-        if(!root) return result;
+    void collecteRightNode(TreeNode* root, int level, vector<int>& result) {
+        if (!root)
+            return;
 
-        queue<TreeNode*>q;
-        q.push(root);
+        if (level == result.size())
+            result.push_back(root->val);
 
-        while(!q.empty())
-        {
-            int n = q.size();
-            TreeNode *curr = nullptr;
-
-            while(n--)
-            {
-                curr = q.front();
-                q.pop();
-
-                if(curr->left) q.push(curr->left);
-                if(curr->right) q.push(curr->right);
-            }
-
-            result.push_back(curr->val);
-        }
-
+        collecteRightNode(root->right, level + 1, result);
+        collecteRightNode(root->left, level + 1, result);
+    }
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        if (!root)
+            return result;
+        collecteRightNode(root, 0, result);
         return result;
-        
     }
 };
+// class Solution {
+// public:
+//     vector<int> rightSideView(TreeNode* root)
+//     {
+//         vector<int>result;
+//         if(!root) return result;
+
+//         queue<TreeNode*>q;
+//         q.push(root);
+
+//         while(!q.empty())
+//         {
+//             int n = q.size();
+//             TreeNode *curr = nullptr;
+
+//             while(n--)
+//             {
+//                 curr = q.front();
+//                 q.pop();
+
+//                 if(curr->left) q.push(curr->left);
+//                 if(curr->right) q.push(curr->right);
+//             }
+
+//             result.push_back(curr->val);
+//         }
+
+//         return result;
+
+//     }
+// };

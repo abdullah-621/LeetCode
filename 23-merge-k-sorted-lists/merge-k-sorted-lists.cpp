b975f6c152e2808dc13eccaa7c73198553dd4298
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-08-26 11:31:45 +0600

    Time: 2 ms (70.25%) | Memory: 18.6 MB (50.93%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -9,38 +9,83 @@
  * };
  */
 class Solution {
-public:
-    void merge(vector<int>&ans, ListNode* head){
-        if(head == nullptr) return;
-        ListNode *temp = head;
-
-        while(temp != nullptr){
-            ans.push_back(temp->val);
-            temp = temp->next;
-        }
-        
-    }
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        vector<int>ans;
-
-        for(auto head : lists)
+public:  
+    class Compare{
+        public:
+        bool operator()(ListNode* a, ListNode* b)
         {
-        merge(ans,head);
+            return a->val > b->val;
+        } 
+    };
+    
+    ListNode* mergeKLists(vector<ListNode*>& lists)
+    {
+        vector<ListNode*>init;
+
+        // check any non-null head 
+        for(auto node : lists)
+        {
+            if(node) 
+            {
+                init.push_back(node);
+            }
         }
 
-        sort(ans.begin(),ans.end());
+        priority_queue<ListNode*, vector<ListNode*>, Compare> pq(init.begin(), init.end());
 
-        ListNode *head = new ListNode(0);
-        ListNode *curr = head;
+        ListNode* root = new ListNode(0); // dummy node
+        ListNode* tail = root;
 
-        for(int i : ans){
-            ListNode *temp = new ListNode(i);
-            curr->next = temp;
-            curr = curr->next;
+        ListNode* temp;
+
+
+        while(!pq.empty())
+        {
+            temp = pq.top();
+            pq.pop();
+
+            tail->next = temp;
+            tail = tail->next;
+
+            if(temp->next)
+            {
+                pq.push(temp->next);
+            }
         }
 
-        return head->next;
-
-
+        return root->next;
     }
 };
+// class Solution {
+// public:
+//     void merge(vector<int>& ans, ListNode* head) {
+//         if (head == nullptr)
+//             return;
+//         ListNode* temp = head;
+
+//         while (temp != nullptr) {
+//             ans.push_back(temp->val);
+//             temp = temp->next;
+//         }
+//     }
+//     ListNode* mergeKLists(vector<ListNode*>& lists) {
+//         vector<int> ans;
+
+//         for (auto head : lists) {
+//             merge(ans, head);
+//         }
+
+//         sort(ans.begin(), ans.end());
+
+//         ListNode* head = new ListNode(0);
+//         ListNode* curr = head;
+
+//         for (int i : ans) {
+//             ListNode* temp = new ListNode(i);
+//             curr->next = temp;
+//             curr = curr->next;
+//         }
+
+//         return head->next;
+//     }
+// };

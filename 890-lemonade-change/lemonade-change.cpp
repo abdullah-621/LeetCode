66d0e02467089468b3214e64f0b14b696189af8f
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-09-17 12:43:20 +0600

    Time: 3 ms (10.29%) | Memory: 87 MB (93.58%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int no_of_5 = 0;
+        int no_of_10 = 0;
+
+
+        for(int i = 0; i < bills.size(); i++)
+        {
+            int note = bills[i];
+
+            if(note == 5)
+            {
+                no_of_5 ++;
+            }
+            else if(note == 10)
+            {
+                no_of_10 ++;
+                if(no_of_5 >= 1)
+                {
+                    no_of_5--;
+                }
+                else 
+                {
+                    return false;
+                }
+            }
+            else if(note == 20)
+            {
+                if(no_of_5 >= 1 && no_of_10 >= 1)
+                {
+                    no_of_5 --;
+                    no_of_10 --;
+                }
+                else if(no_of_5 >= 3)
+                {
+                    no_of_5 -=3;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};

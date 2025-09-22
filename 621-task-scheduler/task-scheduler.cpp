15b5d6c7d535befa3ef0abcb950ad81b778b5372
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-09-22 12:13:30 +0600

    Time: 11 ms (62.61%) | Memory: 38.2 MB (83.94%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,27 +1,55 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        // count freq of all char
-        vector<int>freq(26,0);
-        // calculate higest freq
-        int count = 0;
+        
+        unordered_map<char, int> freq;
 
-        for(int i = 0; i < tasks.size(); i++)
+        int count  = 0; 
+
+        for(char task : tasks)
         {
-            freq[tasks[i] - 'A']++;
-            count = max(count , freq[tasks[i] - 'A']);
+            freq[task]++;
+            count = max(count, freq[task]);
         }
 
         int ans = (count - 1) * (n + 1);
 
-        for(int i = 0; i < 26; i++)
+        for(auto it : freq)
         {
-            if(freq[i] == count)
+            if(it.second == count)
             {
                 ans++;
             }
         }
 
-        return ans > tasks.size() ? ans : tasks.size();
+        return max((int)tasks.size(), ans);
     }
+
 };
+// class Solution {
+// public:
+//     int leastInterval(vector<char>& tasks, int n) {
+//         // count freq of all char
+//         vector<int>freq(26,0);
+//         // calculate higest freq
+//         int count = 0;
+
+//         for(int i = 0; i < tasks.size(); i++)
+//         {
+//             freq[tasks[i] - 'A']++;
+//             count = max(count , freq[tasks[i] - 'A']);
+//         }
+
+//         int ans = (count - 1) * (n + 1);
+
+//         for(int i = 0; i < 26; i++)
+//         {
+//             if(freq[i] == count)
+//             {
+//                 ans++;
+//             }
+//         }
+
+//         return ans > tasks.size() ? ans : tasks.size;
+//     }
+// };

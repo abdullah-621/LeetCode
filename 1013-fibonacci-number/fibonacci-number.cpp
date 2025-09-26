2cfad097c31765faf5f141474438cd736310aa7a
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-09-26 11:49:37 +0600

    Time: 0 ms (100.00%) | Memory: 8.4 MB (11.07%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -11,26 +11,47 @@
 //     }
 // };
 
-
 // Dynamic Programming Memoization(Top-Down)
+// class Solution {
+// public:
+//     int DpMemo(int n, vector<int>&dp)
+//     {
+//         // base case
+//         if(n <= 1) return n;
+
+//         // Already calculated
+//         if(dp[n] != -1)
+//         {
+//          return dp[n];
+//         }
+
+//         return dp[n] = DpMemo(n - 1, dp) + DpMemo(n - 2, dp);
+//     }
+
+//     int fib(int n) {
+//         vector<int>dp(n + 1, -1);
+//         return DpMemo(n, dp);
+//     }
+// };
 class Solution {
 public:
-    int DpMemo(int n, vector<int>&dp)
-    {
-        // base case
-        if(n <= 1) return n;
-
-        // Already calculated
-        if(dp[n] != -1)
+    int fib(int n) {
+        vector<int> dp(n + 1, -1);
+        if(n > 0){
+        dp[0] = 0;
+        dp[1] = 1;
+        }
+        else
         {
-         return dp[n];
+            dp[0] = 0;
         }
 
-        return dp[n] = DpMemo(n - 1, dp) + DpMemo(n - 2, dp);
-    }
+        for (int i = 2; i < dp.size(); i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
 
-    int fib(int n) {
-        vector<int>dp(n + 1, -1);
-        return DpMemo(n, dp);
+        return dp[dp.size() - 1];
     }
 };
+
+// Dynamic Programming Tabulation(Bottom -)

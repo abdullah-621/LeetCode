7e99ec76eaa536828ba73693cc6e341caa899bb6
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-09-26 12:10:24 +0600

    Time: 2 ms (77.47%) | Memory: 7.8 MB (63.69%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -35,22 +35,45 @@
 // };
 
 // Dynamic Programming Tabulation(Bottom - Top)
+// class Solution {
+// public:
+//     int fib(int n) {
+
+//         if (n < 1)
+//             return n;
+
+//         vector<int> dp(n + 1, -1);
+
+//         dp[0] = 0;
+//         dp[1] = 1;
+
+//         for (int i = 2; i < dp.size(); i++) {
+//             dp[i] = dp[i - 1] + dp[i - 2];
+//         }
+
+//         return dp[n];
+//     }
+// };
+
+// More Optimized (space reduce)
 class Solution {
 public:
     int fib(int n) {
 
-        if (n < 1)
+        if (n <= 1)
             return n;
 
-        vector<int> dp(n + 1, -1);
+        int prev = 1;
+        int prev2 = 0;
+        int curr;
 
-        dp[0] = 0;
-        dp[1] = 1;
-
-        for (int i = 2; i < dp.size(); i++) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+        for(int i = 2; i <= n; i++)
+        {
+            curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
         }
 
-        return dp[n];
+        return prev;
     }
 };

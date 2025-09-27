becfa3a48ace1d44606be00e5b32cad1bfd50337
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-09-27 12:46:52 +0600

    Time: 23 ms (98.18%) | Memory: 16.7 MB (42.67%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.cpp
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -1,50 +1,79 @@
+// O(n`2)log(n)
+
+// class Solution {
+// public:
+//     int BinarySearch(vector<int>&nums, int l, int r, int target)
+//     {
+//         int k = -1;
+
+//         while(l <= r)
+//         {
+//             int mid = (r + l) / 2;
+
+//             if(nums[mid] < target)
+//             {
+//                 k = mid;
+//                 l = mid + 1;
+//             }
+//             else{
+//                 r = mid - 1;
+//             }
+//         }
+
+//         return k;
+//     }
+//     int triangleNumber(vector<int>& nums) {
+
+//        sort(nums.begin(), nums.end());
+//        int n = nums.size();
+
+//        int count = 0;
+
+//        for(int i = 0; i < n; i++)
+//        {
+//             if(nums[i] == 0)
+//             continue;
+
+//             for(int j = i + 1; j < n; j++)
+//             {
+//                 int sum = nums[i] + nums[j];
+
+//                 int k = BinarySearch(nums, j + 1, n - 1, sum);
+
+//                 if(k != -1)
+//                 {
+//                     count += (k - j);
+//                 }
+//             }
+//        }
+
+//         return count;
+//     }
+// };
+
+// O(n`2)
 class Solution {
 public:
-    int BinarySearch(vector<int>&nums, int l, int r, int target)
-    {
-        int k = -1;
-
-        while(l <= r)
-        {
-            int mid = (r + l) / 2;
-
-            if(nums[mid] < target)
-            {
-                k = mid;
-                l = mid + 1;
-            }
-            else{
-                r = mid - 1;
-            }
-        }
-
-        return k;
-    }
     int triangleNumber(vector<int>& nums) {
-       
-       sort(nums.begin(), nums.end());
-       int n = nums.size();
 
-       int count = 0;
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
 
-       for(int i = 0; i < n; i++)
-       {
-            if(nums[i] == 0)
-            continue;
+        int count = 0;
 
-            for(int j = i + 1; j < n; j++)
-            {
-                int sum = nums[i] + nums[j];
+        for (int k = n - 1; k >= 2; k--) {
+            int i = 0;
+            int j = k - 1;
 
-                int k = BinarySearch(nums, j + 1, n - 1, sum);
-
-                if(k != -1)
-                {
-                    count += (k - j);
+            while (i < j) {
+                if (nums[i] + nums[j] > nums[k]) {
+                    count += (j - i);
+                    j--;
+                } else {
+                    i++;
                 }
             }
-       }
-
+        }
         return count;
     }
 };

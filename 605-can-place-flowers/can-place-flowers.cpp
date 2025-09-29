f31f5290e700839dae916b5dac7f402689f88ed9
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-09-29 19:27:45 +0600

    Time: 0 ms (100.00%) | Memory: 24 MB (74.15%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -11,6 +11,10 @@ public:
                 bool leftEmpty = (i == 0) || (flowerbed[i - 1] == 0);
                 bool rightEmpty = (i == size - 1) || (flowerbed[i + 1] == 0);
 
+                // can't use this condition because of Segmentation fault (segfault)
+                // bool leftEmpty =(flowerbed[i - 1] == 0) ||  (i == 0);
+                // bool rightEmpty = (flowerbed[i + 1] == 0) || (i == size - 1);
+
                 if(leftEmpty && rightEmpty)
                 {
                     flowerbed[i] = 1;

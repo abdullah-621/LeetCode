e731013074af09dab08f9094b010eb2329b092e8
Abdullah Al Masum <abdullahmasum621@gmail.com>
2025-10-03 12:09:16 +0600

    Time: 0 ms (100.00%) | Memory: 112.5 MB (18.39%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -4,19 +4,13 @@ public:
         
         int totalGas = 0, totalCost = 0;
 
-        for(int i = 0; i < gas.size(); i++)
-        {
-            totalGas += gas[i];
-            totalCost += cost[i];
-        }
-
-        if(totalGas < totalCost) return -1;
-
         int start = 0;
         int currGas = 0;
 
         for(int i = 0; i < gas.size(); i++)
         {
+            totalGas += gas[i];
+            totalCost += cost[i];
             currGas += gas[i] - cost[i];
 
             if(currGas < 0)
@@ -25,6 +19,8 @@ public:
                 start = i + 1;
             }
         }
+
+        if(totalGas < totalCost) return -1;
 
         return start;
 
